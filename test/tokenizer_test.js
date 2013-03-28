08e24454da579273a9189111238467479492971a
Ivan Nikulin <inikulin@gmail.com>
2013-03-28 23:53:04 +0400

    States refactorings

--- test/tokenizer_test.js
+++ test/tokenizer_test.js
@@ -156,9 +156,6 @@ function getFullTestName(test) {
 
 //Here we go..
 loadTests().forEach(function (test) {
-    if (test.index !== 40)
-        return;
-
     exports[getFullTestName(test)] = function (t) {
         var out = tokenize(test.input, test.initialState, test.lastStartTag);
 
