d3005b28506eacbd6371713260e14bd31aa67d64
Ivan Nikulin <ifaaan@gmail.com>
2013-07-18 18:10:15 +0400

    Api fixed. Version changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Parser = require('./lib/parser').Parser,
     HTML = require('./lib/html');
 
 exports.parse = function (html, treeAdapter) {
-    var parser = new Parser(html, treeAdapter);
+    var parser = new Parser(html, null, treeAdapter);
 
     return parser.parse();
 };
