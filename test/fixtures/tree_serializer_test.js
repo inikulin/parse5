956bdb99c364a556cde0b040fdc28abfd9689b73
inikulin <ifaaan@gmail.com>
2014-08-05 17:31:43 +0400

    Doctype serialization refactoring. Fixed new lines rendering in <pre> tag. Regression tests added. Version updated.

--- test/fixtures/tree_serializer_test.js
+++ test/fixtures/tree_serializer_test.js
@@ -5,6 +5,70 @@ var fs = require('fs'),
     testGenerator = require('../test_generator');
 
 
+exports['Regression - SYSTEM-only doctype serialization'] = function (t) {
+    var html = '<!DOCTYPE html SYSTEM "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
+               '<html><head></head><body></body></html>',
+        parser = new Parser(),
+        serializer = new Serializer(),
+        document = parser.parse(html),
+        serializedResult = serializer.serialize(document);
+
+    t.strictEqual(serializedResult, html);
+    t.done();
+};
+
+exports['Regression - Escaping of doctypes with quotes in them'] = function (t) {
+    var htmlStrs = [
+            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" ' +
+            '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
+            '<html><head></head><body></body></html>',
+
+            '<!DOCTYPE html PUBLIC \'-//W3C//"DTD" XHTML 1.0 Transitional//EN\' ' +
+            '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
+            '<html><head></head><body></body></html>',
+
+            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" ' +
+            '\'http://www.w3.org/TR/xhtml1/DTD/"xhtml1-transitional.dtd"\'>' +
+            '<html><head></head><body></body></html>'
+        ],
+        parser = new Parser(),
+        serializer = new Serializer();
+
+    htmlStrs.forEach(function (html) {
+        var document = parser.parse(html),
+            serializedResult = serializer.serialize(document);
+
+        t.strictEqual(serializedResult, html);
+    });
+
+    t.done();
+};
+
+exports['Regression - new line in <pre> tag'] = function (t) {
+    var htmlStrs = [
+            {
+                src: '<!DOCTYPE html><html><head></head><body><pre>\ntest</pre></body></html>',
+                expected: '<!DOCTYPE html><html><head></head><body><pre>test</pre></body></html>'
+            },
+
+            {
+                src: '<!DOCTYPE html><html><head></head><body><pre>\n\ntest</pre></body></html>',
+                expected: '<!DOCTYPE html><html><head></head><body><pre>\n\ntest</pre></body></html>'
+            }
+        ],
+        parser = new Parser(),
+        serializer = new Serializer();
+
+    htmlStrs.forEach(function (htmlStr) {
+        var document = parser.parse(htmlStr.src),
+            serializedResult = serializer.serialize(document);
+
+        t.strictEqual(serializedResult, htmlStr.expected);
+    });
+
+    t.done();
+};
+
 testGenerator.defineForEachTreeAdapter(module.exports, function (_test, adapterName, treeAdapter) {
     function loadTests() {
         var dataDirPath = path.join(__dirname, '../data/tree_serializer'),
@@ -89,7 +153,7 @@ testGenerator.defineForEachTreeAdapter(module.exports, function (_test, adapterN
                 serializedResult = removeNewLines(serializer.serialize(document)),
                 expected = removeNewLines(test.expected);
 
-            //NOTE: use ok assertion, so output will not be polluted by the whole content of the assertable strings
+            //NOTE: use ok assertion, so output will not be polluted by the whole content of the strings
             t.ok(serializedResult === expected, getAssertionMessage(serializedResult, expected));
             t.done();
         };
