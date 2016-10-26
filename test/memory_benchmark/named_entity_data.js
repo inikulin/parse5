8f5564b845bb4cbf7dc606ddc46cd66b1c83613c
inikulin <ifaaan@gmail.com>
2016-10-26 20:54:37 +0300

    Fix linting errors

--- test/memory_benchmark/named_entity_data.js
+++ test/memory_benchmark/named_entity_data.js
@@ -1,11 +1,13 @@
+'use strict';
+
 var format = require('human-format');
 
 (function () {
-    var before = new process.memoryUsage().rss;
+    var before = process.memoryUsage().rss;
 
     require('../../lib/tokenizer/named_entity_trie');
 
-    var after = new process.memoryUsage().rss;
+    var after = process.memoryUsage().rss;
 
     console.log('Initial memory consumption: ', format(after - before, {unit: 'B'}));
 })();
