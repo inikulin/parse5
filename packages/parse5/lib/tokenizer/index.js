parse5/parse5-fork

refactor: Make `C1_CONTROLS_REFERENCE_REPLACEMENTS` a `Map`

packages/parse5/lib/tokenizer/index.js
@@ -8,35 +8,35 @@ const $ = unicode.CODE_POINTS;
 const $$ = unicode.CODE_POINT_SEQUENCES;
 
 //C1 Unicode control character reference replacements
-const C1_CONTROLS_REFERENCE_REPLACEMENTS = {
-    0x80: 0x20ac,
-    0x82: 0x201a,
-    0x83: 0x0192,
-    0x84: 0x201e,
-    0x85: 0x2026,
-    0x86: 0x2020,
-    0x87: 0x2021,
-    0x88: 0x02c6,
-    0x89: 0x2030,
-    0x8a: 0x0160,
-    0x8b: 0x2039,
-    0x8c: 0x0152,
-    0x8e: 0x017d,
-    0x91: 0x2018,
-    0x92: 0x2019,
-    0x93: 0x201c,
-    0x94: 0x201d,
-    0x95: 0x2022,
-    0x96: 0x2013,
-    0x97: 0x2014,
-    0x98: 0x02dc,
-    0x99: 0x2122,
-    0x9a: 0x0161,
-    0x9b: 0x203a,
-    0x9c: 0x0153,
-    0x9e: 0x017e,
-    0x9f: 0x0178,
-};
+const C1_CONTROLS_REFERENCE_REPLACEMENTS = new Map([
+    [0x80, 0x20ac],
+    [0x82, 0x201a],
+    [0x83, 0x0192],
+    [0x84, 0x201e],
+    [0x85, 0x2026],
+    [0x86, 0x2020],
+    [0x87, 0x2021],
+    [0x88, 0x02c6],
+    [0x89, 0x2030],
+    [0x8a, 0x0160],
+    [0x8b, 0x2039],
+    [0x8c, 0x0152],
+    [0x8e, 0x017d],
+    [0x91, 0x2018],
+    [0x92, 0x2019],
+    [0x93, 0x201c],
+    [0x94, 0x201d],
+    [0x95, 0x2022],
+    [0x96, 0x2013],
+    [0x97, 0x2014],
+    [0x98, 0x02dc],
+    [0x99, 0x2122],
+    [0x9a, 0x0161],
+    [0x9b, 0x203a],
+    [0x9c, 0x0153],
+    [0x9e, 0x017e],
+    [0x9f, 0x0178],
+]);
 
 // Named entity tree flags
 const HAS_DATA_FLAG = 1 << 0;
@@ -2111,9 +2111,9 @@ export class Tokenizer {
         } else if (unicode.isControlCodePoint(this.charRefCode) || this.charRefCode === $.CARRIAGE_RETURN) {
             this._err(ERR.controlCharacterReference);
 
-            const replacement = C1_CONTROLS_REFERENCE_REPLACEMENTS[this.charRefCode];
+            const replacement = C1_CONTROLS_REFERENCE_REPLACEMENTS.get(this.charRefCode);
 
-            if (replacement) {
+            if (replacement !== undefined) {
                 this.charRefCode = replacement;
             }
         }

inikulin/parse5

packages/parse5/lib/tokenizer/index.js
@@ -8,35 +8,35 @@ const $ = unicode.CODE_POINTS;
 const $$ = unicode.CODE_POINT_SEQUENCES;
 
 //C1 Unicode control character reference replacements
-const C1_CONTROLS_REFERENCE_REPLACEMENTS = {
-    0x80: 0x20_ac,
-    0x82: 0x20_1a,
-    0x83: 0x01_92,
-    0x84: 0x20_1e,
-    0x85: 0x20_26,
-    0x86: 0x20_20,
-    0x87: 0x20_21,
-    0x88: 0x02_c6,
-    0x89: 0x20_30,
-    0x8a: 0x01_60,
-    0x8b: 0x20_39,
-    0x8c: 0x01_52,
-    0x8e: 0x01_7d,
-    0x91: 0x20_18,
-    0x92: 0x20_19,
-    0x93: 0x20_1c,
-    0x94: 0x20_1d,
-    0x95: 0x20_22,
-    0x96: 0x20_13,
-    0x97: 0x20_14,
-    0x98: 0x02_dc,
-    0x99: 0x21_22,
-    0x9a: 0x01_61,
-    0x9b: 0x20_3a,
-    0x9c: 0x01_53,
-    0x9e: 0x01_7e,
-    0x9f: 0x01_78,
-};
+const C1_CONTROLS_REFERENCE_REPLACEMENTS = new Map([
+    [0x80, 0x20_ac],
+    [0x82, 0x20_1a],
+    [0x83, 0x01_92],
+    [0x84, 0x20_1e],
+    [0x85, 0x20_26],
+    [0x86, 0x20_20],
+    [0x87, 0x20_21],
+    [0x88, 0x02_c6],
+    [0x89, 0x20_30],
+    [0x8a, 0x01_60],
+    [0x8b, 0x20_39],
+    [0x8c, 0x01_52],
+    [0x8e, 0x01_7d],
+    [0x91, 0x20_18],
+    [0x92, 0x20_19],
+    [0x93, 0x20_1c],
+    [0x94, 0x20_1d],
+    [0x95, 0x20_22],
+    [0x96, 0x20_13],
+    [0x97, 0x20_14],
+    [0x98, 0x02_dc],
+    [0x99, 0x21_22],
+    [0x9a, 0x01_61],
+    [0x9b, 0x20_3a],
+    [0x9c, 0x01_53],
+    [0x9e, 0x01_7e],
+    [0x9f, 0x01_78],
+]);
 
 // Named entity tree flags
 const HAS_DATA_FLAG = Math.trunc(1);
@@ -2566,9 +2566,9 @@ export class Tokenizer {
         } else if (unicode.isControlCodePoint(this.charRefCode) || this.charRefCode === $.CARRIAGE_RETURN) {
             this._err(ERR.controlCharacterReference);
 
-            const replacement = C1_CONTROLS_REFERENCE_REPLACEMENTS[this.charRefCode];
+            const replacement = C1_CONTROLS_REFERENCE_REPLACEMENTS.get(this.charRefCode);
 
-            if (replacement) {
+            if (replacement !== undefined) {
                 this.charRefCode = replacement;
             }
         }