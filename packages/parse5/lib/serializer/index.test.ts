9424821eed4bbcd106ac5b5eaec7e8fe051f5f3e
Felix Böhm <188768+fb55@users.noreply.github.com>
2022-01-18 14:46:45 +0000

    Move serializer tests to data file

--- packages/parse5/lib/serializer/index.test.ts
+++ packages/parse5/lib/serializer/index.test.ts
@@ -1,6 +1,5 @@
 import * as assert from 'node:assert';
 import * as parse5 from 'parse5';
-import outdent from 'outdent';
 import { generateSerializerTests } from 'parse5-test-utils/utils/generate-serializer-tests.js';
 import { treeAdapters } from 'parse5-test-utils/utils/common.js';
 import { type Element, isElementNode } from 'parse5/dist/tree-adapters/default';
@@ -21,32 +20,6 @@ describe('serializer', () => {
             };
 
             parse5.serialize(document, { treeAdapter });
-        });
-    });
-
-    describe('Scripting flag (GH-332)', () => {
-        it('should serialize with the scripting flag', () => {
-            const document = parse5.parse('&amp;<noscript>&amp;</noscript>');
-            expect(parse5.serialize(document, { scriptingEnabled: false })).toBe(
-                '<html><head></head><body>&amp;<noscript>&amp;amp;</noscript></body></html>'
-            );
-            expect(parse5.serialize(document, { scriptingEnabled: true })).toBe(
-                '<html><head></head><body>&amp;<noscript>&amp;</noscript></body></html>'
-            );
-        });
-    });
-
-    describe('Mixed content (GH-333)', () => {
-        it('should serialize mixed content', () => {
-            const input = outdent`
-              <svg><style>&lt;</style></svg>
-              <style>&lt;</style>
-
-              <svg><script>&lt;</script></svg>
-              <script>&lt;</script>
-            `;
-            const document = parse5.parse(input);
-            expect(parse5.serialize(document)).toContain(input);
         });
     });
 
