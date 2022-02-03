a85f632e82e9e0151862d5b412c64ebd3dee9c36
Felix Böhm <188768+fb55@users.noreply.github.com>
2022-02-03 13:35:02 -0600

    Use tokenizer events for `generateTokenizationTests`
    This allows us to start asserting expected values as we walk through the document. This will give us stack traces for all future errors.
    
    Improve generate tokenization tests imports

--- packages/parse5-sax-parser/test/parser-feedback-simulator.test.ts
+++ packages/parse5-sax-parser/test/parser-feedback-simulator.test.ts
@@ -1,14 +1,11 @@
-import { Token } from 'parse5/dist/common/token.js';
-import { QueuedHandler } from 'parse5/dist/tokenizer/index.js';
 import { generateTokenizationTests } from 'parse5-test-utils/utils/generate-tokenization-tests.js';
 import { ParserFeedbackSimulator } from '../lib/parser-feedback-simulator.js';
 
 const feedbackPath = new URL('../../../test/data/parser-feedback', import.meta.url);
 
-generateTokenizationTests('ParserFeedbackSimulator', 'ParserFeedbackSimulator', feedbackPath.pathname, () => {
-    const handler = new QueuedHandler();
-    const feedbackSimulator = new ParserFeedbackSimulator({}, handler);
-    const { tokenizer } = feedbackSimulator;
-
-    return { tokenizer, getNextToken: (): Token => handler.getNextToken(tokenizer) };
-});
+generateTokenizationTests(
+    'ParserFeedbackSimulator',
+    'ParserFeedbackSimulator',
+    feedbackPath.pathname,
+    (handler) => new ParserFeedbackSimulator({}, handler).tokenizer
+);
