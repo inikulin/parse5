6a6daf417edce50cc8982adb4bdca6db77c09ed8
Felix Böhm <188768+fb55@users.noreply.github.com>
2022-03-11 10:59:02 +0000

    test: Add upstream html5lib-tests
    And use them for the tokenizer, benchmarks & to generate feedback tests.

--- packages/parse5/lib/tokenizer/index.test.ts
+++ packages/parse5/lib/tokenizer/index.test.ts
@@ -1,4 +1,3 @@
-import * as parse5 from 'parse5';
 import { Tokenizer } from 'parse5/dist/tokenizer/index.js';
 import { generateTokenizationTests } from 'parse5-test-utils/utils/generate-tokenization-tests.js';
 
@@ -8,10 +7,3 @@ const tokenizerOpts = {
 };
 
 generateTokenizationTests('Tokenizer', dataPath.pathname, (handler) => new Tokenizer(tokenizerOpts, handler));
-
-describe('tokenizer', () => {
-    it('Regression - `<<` in comment parses correctly (GH-325)', () => {
-        const document = parse5.parse('<!--<<-->');
-        expect(document.childNodes[0]).toHaveProperty('data', '<<');
-    });
-});
