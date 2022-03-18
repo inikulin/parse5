defd3f3db8a401a3617fe99c22fcebcb6e7f7f5d
fb55 <fb55@users.noreply.github.com>
2022-03-18 14:33:14 +0000

    deploy: 7a191adf5484232e3d3c937882f126f23db43077

--- packages/parse5-sax-parser/lib/dev-null-stream.ts
+++
@@ -1,7 +0,0 @@
-import { Writable } from 'node:stream';
-
-export class DevNullStream extends Writable {
-    override _write(_chunk: string, _encoding: string, cb: () => void): void {
-        cb();
-    }
-}
