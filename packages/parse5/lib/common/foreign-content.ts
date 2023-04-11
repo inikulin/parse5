23bbd1088dd64a993d8c5eab68053c56a185c04c
Felix <188768+fb55@users.noreply.github.com>
2023-04-11 19:07:32 +0100

    s/xml:space/xml:base
    Removed the wrong attribute

--- packages/parse5/lib/common/foreign-content.ts
+++ packages/parse5/lib/common/foreign-content.ts
@@ -81,8 +81,8 @@ const XML_ATTRS_ADJUSTMENT_MAP = new Map([
     ['xlink:show', { prefix: 'xlink', name: 'show', namespace: NS.XLINK }],
     ['xlink:title', { prefix: 'xlink', name: 'title', namespace: NS.XLINK }],
     ['xlink:type', { prefix: 'xlink', name: 'type', namespace: NS.XLINK }],
-    ['xml:base', { prefix: 'xml', name: 'base', namespace: NS.XML }],
     ['xml:lang', { prefix: 'xml', name: 'lang', namespace: NS.XML }],
+    ['xml:space', { prefix: 'xml', name: 'space', namespace: NS.XML }],
     ['xmlns', { prefix: '', name: 'xmlns', namespace: NS.XMLNS }],
     ['xmlns:xlink', { prefix: 'xmlns', name: 'xlink', namespace: NS.XMLNS }],
 ]);
